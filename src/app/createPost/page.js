0244fce81f65547e0feb2d86a8092ca4fa7c832b
Aaron Berkness <aaronberkness@gmail.com>
2025-10-14 19:10:16 -0700

    Before bed

--- src/app/createPost/page.js
+++ src/app/createPost/page.js
@@ -1,11 +1,33 @@
+"use client";
+
 import "../mainStyle.css";
+import React, { useState } from 'react';
 
 export default function CreatePost() {
+  const [text, setText] = useState('');
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+
   return (
     <>
       <h1>
         Create New Post
       </h1>
+
+      <hr />
+
+      <form>
+        <textarea
+          id="multiline-text"
+          value={text}
+          onChange={handleChange}
+          rows={5} // Optional: Specifies the visible number of lines
+          cols={40} // Optional: Specifies the visible width in average character widths
+          placeholder="Type your message here..." // Optional: Placeholder text
+        />
+      </form>
     </>
   );
 }
